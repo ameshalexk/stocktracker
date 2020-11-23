f591cecbdedc26c7da33b56984f6d328f612d1e9
ameshalexk <ameshalexusa@gmail.com>
2020-11-23 00:08:33 -0500

    tracking functionality done with console log

--- monitor.js
+++ monitor.js
@@ -9,7 +9,10 @@ const rand_url =
   'https://www.walmart.com/ip/Nintendo-Switch-Lite-Console-Turquoise/306029956';
 
 async function initBrowser() {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: false,
+    // args: ['--no-sandbox', '--disable-gpu'],
+  });
   const page = await browser.newPage();
   await page.goto(rand_url);
   return page;
@@ -18,7 +21,17 @@ async function initBrowser() {
 async function checkStock(page) {
   await page.reload();
   let content = await page.evaluate(() => document.body.innerHTML);
-  console.log(content);
+  $("link[itemprop='availability']", content).each(function () {
+    let out_of_stock = $(this)
+      .attr('href')
+      .toLowerCase()
+      .includes('outofstock');
+    if (out_of_stock) {
+      console.log('out of stock');
+    } else {
+      console.log('In stock');
+    }
+  });
 }
 
 async function monitor() {
